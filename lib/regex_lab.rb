0faa9fc17af8182113b5ff86eff091a680fdd4d3
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-20 21:57:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -27,9 +27,9 @@ end
 
 def valid_phone_number?(phone)
   if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
-    true
+    false 
   else
-    false
+    true 
   end
 end
 
